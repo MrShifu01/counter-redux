1a0e6705d42b94f3365557d0d4bbe916ca339bf8
ChristianStander <stander.christian@gmail.com>
2023-05-19 09:35:09 +0200

    counter app with redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './store/counter';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState(0)
+
+  const count = useSelector((state) => state.counter.value)
+
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(incrementByAmount(Number(userInput)))
+    setUserInput(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>{count}</h1>
+      </div>
+      <div className='Buttons'>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+
+      <form className='Form' onClick={handleSubmit}>
+        <label>
+          Value:
+          <input
+          type='text'
+          name="value"
+          onChange={(e) => setUserInput(e.target.value)}
+          value={userInput}
+          />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+
     </div>
   );
 }
